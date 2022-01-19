ae36766189d7825ecf652d8a89e1b24223bad54b
Rushdi Bahadoor <rushdi@vpc-direct-service.com>
2022-01-19 10:03:43 +0400

    Modif neworder - afficher details client

---
+++ resources/js/components/detailing/DetailingItemType.vue
@@ -0,0 +1,44 @@
+<template>
+    <transition
+            enter-active-class="animate__animated animate__fadeIn"
+    >
+        <div class="container-fluid h-100 bg-color" v-if="showcontainer">
+           <main-header></main-header>
+            <div class="row d-flex align-content-stretch align-items-stretch flex-row hmax">
+                <side-bar></side-bar>
+                <div class="col  p-0 m-0">
+                    <bread-crumb :paths="paths"></bread-crumb>
+                    <div class="main-view-2 row">
+                        <div class="col">
+                        <h2 class="subtitle">Choose item type</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </transition>
+
+</template>
+<script>
+    //import MainHeader from '../layout/MainHeader';
+    import BreadCrumb from '../layout/BreadCrumb';
+    import SideBar from '../layout/SideBar';
+    import {ref} from 'vue';
+export default {
+    name:"DetailingItemType",
+    components:{BreadCrumb,SideBar},
+    setup() {
+        const showcontainer=ref(true);
+
+        const paths=ref([{name:'All orders',route:'LandingPage'},{name:'Order no',route:'NewOrder'},{name:'Detailing',route:'DetailingItemType'}]);
+
+        return {
+            showcontainer,
+            paths
+        };
+    },
+}
+</script>
+<style scoped>
+
+</style>
