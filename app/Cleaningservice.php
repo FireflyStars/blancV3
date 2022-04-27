d0013124f3e11d9f5be6deba7325db08ae69299a
Rushdi Bahadoor <rushdi@vpc-direct-service.com>
2022-04-27 12:17:20 +0400

    Voyager missing models

---
+++ app/Cleaningservice.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Cleaningservice extends Model{
+    use HasFactory;
+
+    protected $table="cleaningservices";
+}
