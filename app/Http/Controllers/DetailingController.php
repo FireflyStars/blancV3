d80ed9883e5776ab7900147db4da542890840e8f
Rushdi Bahadoor <rushdi@vpc-direct-service.com>
2022-06-13 15:19:03 +0400

    Verif si tracking est parmi les detailingitem pending

--- app/Http/Controllers/DetailingController.php
+++ app/Http/Controllers/DetailingController.php
@@ -927,14 +927,23 @@ class DetailingController extends Controller
                 ->get();
 
             if(count($inv)==0){
-                $err = "HSL $tracking already linked with another customer";
+                $err = "HSL $tracking already linked with another customer.";
             }
         }
 
+        $has_detailing_order = DB::table('detailingitem')->where('tracking',$tracking)
+            ->where('status','In Process')
+            ->latest('id')
+            ->first();
+
+        if($has_detailing_order){
+            $err = "HSL $tracking is already being detailed.";
+        }
 
         return response()->json([
             'item'=>$item,
             'err'=>$err,
+            'has_detailing_order'=>$has_detailing_order,
             //'post'=>$request->all(),
         ]);
     }
