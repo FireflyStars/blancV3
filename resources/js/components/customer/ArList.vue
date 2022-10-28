69766040b2a0ae023b25013d38cc2d9120bcf4b9
Rushdi Bahadoor <rushdi@vpc-direct-service.com>
2022-10-28 14:51:57 +0400

    Correction unpaid orders Modif AR Invoiced - 2 emails + affichage

--- resources/js/components/customer/ArList.vue
+++ resources/js/components/customer/ArList.vue
@@ -379,6 +379,7 @@ export default {
 
                 }).finally(()=>{
                     store.dispatch(`${LOADER_MODULE}${HIDE_LOADER}`);
+                    loadCustomer();
                 });
 
             }
