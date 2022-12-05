9ffec38eec03bf5b44feb3e21fe8f20d2f95c22d
FireflyStars <gotodev7@gmail.com>
2022-12-05 17:18:22 +0800

    Fixed voucher excel export issue

--- app/Exports/Voucher/VoucherDetailSheet.php
+++ app/Exports/Voucher/VoucherDetailSheet.php
@@ -34,7 +34,7 @@ class VoucherDetailSheet implements FromCollection, WithTitle, WithHeadings, Wit
                             'infoCustomer.Name', 'infoCustomer.EmailAddress', 'infoCustomer.LastName', 'infoCustomer.FirstName'
                         )
                         ->whereNotIn('infoOrder.status', [ 'DELETE', 'IN DETAILING', 'VOID'])
-                        ->whereBetween('vouchers_histories.created_at', $this->period)
+                        // ->whereBetween('vouchers_histories.created_at', $this->period)
                         ->get();
         return $data;
     }
